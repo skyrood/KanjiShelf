9c74ed44493b55dce948d59a08e05c303576c9cd
Rodion Kim <skyrood2@yandex.ru>
2025-10-08 19:34:12 +0900

    Initial Commit

---
+++ KanjiShelf/KanjiShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanjiShelfApp.swift
+//  KanjiShelf
+//
+//  Created by Rodion Kim on 2025/10/08.
+//
+
+import SwiftUI
+
+@main
+struct KanjiShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
